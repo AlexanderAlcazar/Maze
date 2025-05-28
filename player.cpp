439ca90321fab1328d7b300723c5ab55044c08c2
Alexander Alcazar <alcazar_alexander_am@student.smc.edu>
2025-05-28 16:34:49 -0700

    90%. Evaluate edge cases.

--- player.cpp
+++ player.cpp
@@ -9,36 +9,35 @@
 //  Player( ... )
 //		Constructs and initializes the Player/Entity and its member variables
 //		Remembers and discovers the starting room.
-Player::Player(Maze* maze, Room p, std::string name, char sprite, const bool tbacktrack)
-	:Entity(maze, p, name, sprite),
-	m_lookingPaper(),
-	m_discoveredRooms(),
-	m_btStack(),
-	BACKTRACKENABLED(tbacktrack) {
-
-	// Discover the starting room
-	m_discoveredRooms.add_front(p);
-	m_lookingPaper.push(p);
+Player::Player(Maze *maze, Room p, std::string name, char sprite, const bool tbacktrack)
+    : Entity(maze, p, name, sprite),
+      m_lookingPaper(),
+      m_discoveredRooms(),
+      m_btStack(),
+      BACKTRACKENABLED(tbacktrack) {
+    // Discover the starting room
+    m_discoveredRooms.add_front(p);
+    m_lookingPaper.push(p);
 }
 
-//  stuck() 
+//  stuck()
 //		See if the player is stuck in the maze (no solution)
 bool Player::stuck() const {
-	return Entity::state() == State::NOEXIT;
+    return Entity::state() == State::NOEXIT;
 }
 
 //  foundExit()
 //		See if the player has found the exit
 bool Player::foundExit() const {
-	return Entity::state() == State::EXIT;
+    return Entity::state() == State::EXIT;
 }
 
 //  getTargetRoom()
 //		Get the room the player wants to look around next.
 //      If m_lookingPaper is empty then return an invalid room.
 Room Player::getTargetRoom() const {
-	if (m_lookingPaper.empty()) return Room(-1, -1);
-	return m_lookingPaper.peek();
+    if (m_lookingPaper.empty()) return Room(-1, -1);
+    return m_lookingPaper.peek();
 }
 
 
@@ -46,134 +45,118 @@ Room Player::getTargetRoom() const {
 //		What does the player say?
 //		Already Implemented, nothing to do here
 void Player::say() {
+    // Freedom supercedes being eaten
+    if (state() == State::EXIT) {
+        std::cout << name() << ": WEEEEEEEEE!";
+        return;
+    }
 
-	// Freedom supercedes being eaten
-	if (state() == State::EXIT) {
-		std::cout << name() << ": WEEEEEEEEE!";
-		return;
-	} 
- 
-	// Being eaten supercedes being lost
-	switch (interact()) {
-	case Interact::ATTACK:
-		std::cout << name() << ": OUCH!";
-		break;
-	case Interact::GREET:
-		break;
-	case Interact::ALONE:
-	default:
-		switch (state()) {
-		case State::LOOK:
-			std::cout << name() << ": Where is the exit?";
-			break;
-		case State::NOEXIT:
-			std::cout << name() << ": Oh no! I am Trapped!";
-			break;
-		case State::BACKTRACK:
-			std::cout << name() << ": Got to backtrack...";
-			break;
-		default:
-			std::cout << name() << ": A - hee - ahee ha - hee!";
-			break;
-		}
+    // Being eaten supercedes being lost
+    switch (interact()) {
+        case Interact::ATTACK:
+            std::cout << name() << ": OUCH!";
+            break;
+        case Interact::GREET:
+            break;
+        case Interact::ALONE:
+        default:
+            switch (state()) {
+                case State::LOOK:
+                    std::cout << name() << ": Where is the exit?";
+                    break;
+                case State::NOEXIT:
+                    std::cout << name() << ": Oh no! I am Trapped!";
+                    break;
+                case State::BACKTRACK:
+                    std::cout << name() << ": Got to backtrack...";
+                    break;
+                default:
+                    std::cout << name() << ": A - hee - ahee ha - hee!";
+                    break;
+            }
 
-		break;
-	}
+            break;
+    }
 }
 
 
-//  update() 
+//  update()
 //		This function implements an algorithm to look through the maze
-//      for places to move (STATE::LOOK). update also handles moving the 
-//		player as well, if there is an open undiscovered cell to move to the  
-//		player just moves there.  However, there will be cases in the next 
-//		desired room to look around is not adjacent (more than one cell away), 
-//		peek this room the player must backtrack (State::BACKTRACK) to a room 
+//      for places to move (STATE::LOOK). update also handles moving the
+//		player as well, if there is an open undiscovered cell to move to the
+//		player just moves there.  However, there will be cases in the next
+//		desired room to look around is not adjacent (more than one cell away),
+//		peek this room the player must backtrack (State::BACKTRACK) to a room
 //		that is adjacent to the next room the player was planning to visit
 //
-//		The player can only do one thing each call, they can either LOOK or 
+//		The player can only do one thing each call, they can either LOOK or
 //		BACKTRACK not both.  Nor should the player move more than one cell per
 //		call to update.  If you examine the function calls for the entire game
 //		you should observe that this is already be called within a loop. Your
-//		Implementation should NOT have any loops that pertain to actual movement 
-//		of the player.  
+//		Implementation should NOT have any loops that pertain to actual movement
+//		of the player.
 //
 //		Backtracking is challenging, save it for the very very very last thing.
 //		Make sure the STATE::LOOK aspect compiles and works first.
 // Update function
-void Player::update() { 
+void Player::update() {
+    if (BACKTRACKENABLED) {
+        // Set by the settings file, if BACKTRACKENABLED is false, then
+        // your program should behave exactly as seen in the slides or
+        // example executables (with teleporting).
+        // You may have multiple branching statements like this.
+        // if(BACKTRACKENABLED) { ... code relating to backtracking
+    }
+    switch (state()) {
+        case State::EXIT:
+            return;
+        case State::NOEXIT:
+            return;
+        case State::BACKTRACK: {
+            Room previous = m_btStack.peek();
+            m_btStack.pop();
+            move(previous);
 
-	if (BACKTRACKENABLED) {
-		// Set by the settings file, if BACKTRACKENABLED is false, then
-		// your program should behave exactly as seen in the slides or
-		// example executables (with teleporting).
-		// You may have multiple branching statements like this.
-		// if(BACKTRACKENABLED) { ... code relating to backtracking
+            bool adjacent = room().adjacent(m_lookingPaper.peek());
+            if (m_btStack.empty() || adjacent) {
+                state(State::LOOK);
+                return;
+            }
+            break;
+        }
+        case State::LOOK: {
+            Room target = getTargetRoom();
+            m_lookingPaper.pop();
+            if (BACKTRACKENABLED)
+                m_btStack.push(room());
+            move(target);
 
-		bool adjacent = room().adjacent(m_lookingPaper.peek());
+            if (maze()->foundExit(target)) {
+                state(State::EXIT);
+                return;
+            }
 
-		if (!adjacent)
-			state(State::BACKTRACK);
+            Room direction[] = {
+                target + Room(-1, 0), target + Room(1, 0),
+                target + Room(0, -1), target + Room(0, 1)
+            };
+            for (int i = 0; i < 4; i++) {
+                if (maze()->open(direction[i]) &&
+                    m_discoveredRooms.search(direction[i]) == -42) {
+                    m_lookingPaper.push(direction[i]);
+                    m_discoveredRooms.add_front(direction[i]);
+                }
+            }
 
-	}
+            if (m_lookingPaper.empty()) {
+                state(State::NOEXIT);
+                return;
+            }
 
-	if (state() == State::BACKTRACK) {
-		if (room() == m_btStack.peek())
-			m_btStack.pop();
-		Room target = m_btStack.peek();
-		m_btStack.pop();
-		move(target);
-		if (m_btStack.empty()) {
-			state(State::LOOK);
-			return;
-		}
-	}
-	if(state() == State::LOOK) {
-		//when player is constructed, m_lookingPaper is initialized with one room
-		Room target = getTargetRoom();
-		m_lookingPaper.pop();
-		if (BACKTRACKENABLED)
-			m_btStack.push(target);
-		move(target);
-
-		if (maze()->foundExit(target)) {
-			state(State::EXIT);
-			return;
-		}
-
-		Room west = target + Room(-1, 0);
-		Room east = target + Room(1, 0);
-		Room north = target + Room(0, -1);
-		Room south = target + Room(0, 1);
-		if(maze()->open(west)){
-			if(m_discoveredRooms.search(west) == -42){
-				m_lookingPaper.push(west);
-				m_discoveredRooms.add_front(west);
-			}
-		}
-		if(maze()->open(east)){
-			if(m_discoveredRooms.search(east) == -42){
-				m_lookingPaper.push(east);
-				m_discoveredRooms.add_front(east);
-			}
-		}
-		if(maze()->open(north)){
-			if(m_discoveredRooms.search(north) == -42){
-				m_lookingPaper.push(north);
-				m_discoveredRooms.add_front(north);
-			}
-		}
-		if(maze()->open(south)){
-			if(m_discoveredRooms.search(south) == -42){
-				m_lookingPaper.push(south);
-				m_discoveredRooms.add_front(south);
-			}
-		}
-		if (m_lookingPaper.empty()) {
-			state(State::NOEXIT);
-			return;
-		}
-	}
-
-
+            bool adjacent = room().adjacent(m_lookingPaper.peek());
+            if (!adjacent)
+                state(State::BACKTRACK);
+            break;
+        }
+    }
 }
